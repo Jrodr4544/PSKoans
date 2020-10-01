034914531ead9232a2d35705c65beee54cfc5289
Jelson Rodriguez <maskdbandit@gmail.com>
2020-09-30 20:51:36 -0400

    learned about comparison object

--- PSKoans/Koans/Cmdlets 1/AboutCompareObject.Koans.ps1
+++ PSKoans/Koans/Cmdlets 1/AboutCompareObject.Koans.ps1
@@ -15,9 +15,9 @@ Describe 'Compare-Object' {
             DifferenceObject = 'these', 'is', 'the', 'difference'
         }
         $CompareData = Compare-Object @Comparison
-        '__' | Should -Be $CompareData[0].InputObject
-        '__' | Should -Be $CompareData[0].SideIndicator
-        $CompareData[0] | Should -BeOfType __
+        'these' | Should -Be $CompareData[0].InputObject
+        '=>' | Should -Be $CompareData[0].SideIndicator
+        $CompareData[0] | Should -BeOfType System.Object
     }
 
     It 'can display items that occur in both sets' {
@@ -28,6 +28,6 @@ Describe 'Compare-Object' {
         }
         $CompareData = Compare-Object @Comparison -IncludeEqual
         $CompareData.SideIndicator -contains '==' | Should -BeTrue
-        '__' | Should -Be $CompareData.Where{$_.InputObject -eq 'the'}.SideIndicator
+        '==' | Should -Be $CompareData.Where{$_.InputObject -eq 'the'}.SideIndicator
     }
 }
